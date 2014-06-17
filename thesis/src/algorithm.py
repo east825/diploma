b05a8f5e7a89ed2fbe4828d1d856d9df1dfbb95c
Mikhail Golubev <qsolo825@gmail.com>
2014-06-17 04:11:36 +0400

    Slightly clean up algorithm in appendixes and reports samples

--- thesis/src/algorithm.py
+++ thesis/src/algorithm.py
@@ -2,10 +2,6 @@ def suggest_classes(self, accessed_attrs):
     def unite(sets):
         return functools.reduce(set.union, sets, set())
 
-    def intersect(sets):
-        return functools.reduce(set.intersection, sets) if sets else set()
-
-    # More fair algorithm because it considers newly discovered bases classes as well
     index = self.indexes['CLASS_ATTRIBUTE_INDEX']
     candidates = unite(index[attr] for attr in accessed_attrs)
 
@@ -18,15 +14,10 @@ def suggest_classes(self, accessed_attrs):
         checked.add(candidate)
         bases = self._resolve_bases(candidate)
 
-        # register number of base classes for statistics
-        self.statistics['max_bases'].set_max(len(bases), candidate.qname)
-
         available_attrs = unite(b.attributes for b in bases) | candidate.attributes
         if accessed_attrs <= available_attrs:
             suitable.add(candidate)
 
-        # new classes could be added to index during call to _resolve_bases(),
-        # so we have to check them as well
         if not self.config['FOLLOW_IMPORTS']:
             for base in bases:
                 if base in checked:
@@ -34,7 +25,6 @@ def suggest_classes(self, accessed_attrs):
                 if any(attr in base.attributes for attr in accessed_attrs):
                     candidates.add(base)
 
-    # remove subclasses if their superclasses is suitable also
     for cls in suitable.copy():
         if any(base in suitable for base in self._resolve_bases(cls)):
             suitable.remove(cls)
